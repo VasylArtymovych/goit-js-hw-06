30f9ecdfdee159464be4aa4ab44a934dc1e7eb2d
VasylArtymovych <art777vasss@gmail.com>
2022-03-18 22:11:55 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createlistItem = (item) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('item');
+  listItem.textContent = item;
+  return listItem;
+}
+
+const listItems = ingredients.map(createlistItem);
+
+const ingredientsListRef = document.querySelector('#ingredients');
+ingredientsListRef.append(...listItems);
